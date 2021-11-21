a97cc173330fd7ae411ff15fa39e3e8dd16dc905
christopher-besch <christopher.besch@gmx.de>
2021-11-21 21:48:25 +0100

    started submission

---
+++ submission/aufgabe1/src/main.cpp
@@ -0,0 +1,275 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+void print_shifts(const std::vector<int>& move_possible, const std::vector<std::vector<int>>& shift, int cars_amount, char first_car)
+{
+    // print heading
+    std::cout << "name possible moves shifts" << std::endl;
+    std::cout << "==== ============== ======" << std::endl;
+    std::cout << "                    ";
+    for(int i = 0; i < cars_amount; ++i)
+        std::cout << static_cast<char>(first_car + i) << " ";
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    // print table
+    for(int i = 0; i < cars_amount; ++i) {
+        std::cout << static_cast<char>(first_car + i) << "    " << move_possible[i] << "              ";
+        for(int y = 0; y < cars_amount; ++y) {
+            std::cout << shift[i][y] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// merge source into target
+void update_row_dir(const std::vector<int>& source, std::vector<int>& target)
+{
+    for(int i = 0; i < target.size(); ++i)
+        target[i] = std::max(target[i], source[i]);
+}
+// merge reduced source into target
+void update_row_red(const std::vector<int>& source, std::vector<int>& target)
+{
+    for(int i = 0; i < target.size(); ++i)
+        target[i] = std::max(target[i], source[i] - 1);
+}
+
+void load_possible_moves(
+    const std::vector<std::pair<int, char>>& cross_cars,
+    int                                      cars_amount,
+    std::vector<std::vector<int>>&           shift,
+    std::vector<int>&                        move_possible,
+    bool                                     go_left)
+{
+    int start = go_left ? 0 : cross_cars.size() - 1;
+    int end   = go_left ? cross_cars.size() : -1;
+    for(int c = start; c != end; go_left ? ++c : --c) {
+        int first, second, next, next_but_one;
+        if(go_left) {
+            // location of cross_car
+            // this pace gets primarily moved
+            first = cross_cars[c].first;
+            // secondary move
+            second = cross_cars[c].first + 1;
+            // places left or right of cross_car
+            next         = first - 1;
+            next_but_one = first - 2;
+        }
+        else {
+            first        = cross_cars[c].first + 1;
+            second       = cross_cars[c].first;
+            next         = first + 1;
+            next_but_one = first + 2;
+        }
+        // check if can be moved twice
+        if(next_but_one >= 0 &&
+           next_but_one < cars_amount &&
+           move_possible[next] == 2 &&
+           move_possible[next_but_one] >= 1) {
+            // can be moved twice
+            move_possible[first]  = 2;
+            move_possible[second] = 2;
+            // this car obviously has to be moved
+            shift[first][first]   = 2;
+            shift[first][second]  = 2;
+            shift[second][first]  = 2;
+            shift[second][second] = 2;
+
+            // this one always has to be moved two paces
+            update_row_dir(shift[next], shift[first]);
+            update_row_dir(shift[next], shift[second]);
+            // only reduce when one too much
+            if(move_possible[next_but_one] == 2) {
+                // right one only has to move once
+                // only half movement required
+                update_row_red(shift[next_but_one], shift[first]);
+                update_row_red(shift[next_but_one], shift[second]);
+            }
+            else {
+                // full movement required
+                update_row_dir(shift[next_but_one], shift[first]);
+                update_row_dir(shift[next_but_one], shift[second]);
+            }
+        }
+        // can be moved once at least?
+        else if(next >= 0 &&
+                next < cars_amount &&
+                move_possible[next] >= 1) {
+            // can be moved once
+            move_possible[first]  = 1;
+            move_possible[second] = 1;
+
+            // obviously has to be moved once
+            shift[first][first]   = 1;
+            shift[first][second]  = 1;
+            shift[second][first]  = 1;
+            shift[second][second] = 1;
+
+            if(move_possible[next] == 2) {
+                update_row_red(shift[next], shift[first]);
+                update_row_red(shift[next], shift[second]);
+            }
+            else {
+                update_row_dir(shift[next], shift[first]);
+                update_row_dir(shift[next], shift[second]);
+            }
+        }
+        // can't be moved
+        else {
+            move_possible[first]  = 0;
+            move_possible[second] = 0;
+        }
+    }
+}
+
+// when is_first -> car has to be moved twice
+// return true when can be moved in that direction
+bool get_required_shifts(int moves_possible, std::vector<int>& shift, bool is_first, std::vector<int>& shift_req)
+{
+    // can be moved once and only have to move once <- i is second part of cross car
+    if(moves_possible == 1 && !is_first) {
+        update_row_dir(shift, shift_req);
+        return true;
+    }
+    // can be moved twice but only has to be moved once
+    else if(moves_possible == 2 && !is_first) {
+        update_row_red(shift, shift_req);
+        return true;
+    }
+    // can be moved twice and has to be moved twice
+    else if(moves_possible == 2 && is_first) {
+        update_row_dir(shift, shift_req);
+        return true;
+    }
+    return false;
+}
+
+// which shift moves fewer cross cars?
+bool better_shift(const std::vector<int>& a, const std::vector<int>& b)
+{
+    int count_a {0}, count_b {0};
+    int moves_a {0}, moves_b {0};
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i]) {
+            ++count_a;
+            moves_a += a[i];
+        }
+        if(b[i]) {
+            ++count_b;
+            moves_b += b[i];
+        }
+    }
+    if(count_a < count_b)
+        return true;
+    if(count_a > count_b)
+        return false;
+    if(moves_a < moves_b)
+        return true;
+    if(moves_a > moves_b)
+        return false;
+    return true;
+}
+
+int main()
+{
+    // read input
+    char first_car, last_car;
+    std::cin >> first_car >> last_car;
+    int cars_amount = last_car - first_car + 1;
+    // turns location into car name
+    // name, left part of cross car?
+    std::vector<std::pair<char, bool>> cross_car_lookup(cars_amount);
+
+    int cross_car_amount;
+    std::cin >> cross_car_amount;
+    // location, name
+    std::vector<std::pair<int, char>> cross_cars(cross_car_amount);
+    for(int i = 0; i < cross_car_amount; ++i) {
+        // paces of car
+        int  left, right;
+        char name;
+        std::cin >> name >> left;
+        right                   = left + 1;
+        cross_cars[i].first     = left;
+        cross_cars[i].second    = name;
+        cross_car_lookup[left]  = {name, true};
+        cross_car_lookup[right] = {name, false};
+    }
+    std::sort(cross_cars.begin(), cross_cars.end());
+
+    // 2 -> can be moved two paces
+    // 1 -> can only be moved once
+    // 0 -> unable to be moved at all
+    // initially all cars can be moved as far as possible
+    std::vector<int> move_left_possible(cars_amount, 2);
+    std::vector<int> move_right_possible(cars_amount, 2);
+
+    // 0 -> doesn't have to be moved
+    // 1 -> has to be moved once
+    // 2 -> has to be moved twice
+    // initially no cars have to be moved
+    std::vector<std::vector<int>> left_shift(cars_amount, std::vector<int>(cars_amount, 0));
+    std::vector<std::vector<int>> right_shift(cars_amount, std::vector<int>(cars_amount, 0));
+
+    load_possible_moves(cross_cars, cars_amount, left_shift, move_left_possible, true);
+    load_possible_moves(cross_cars, cars_amount, right_shift, move_right_possible, false);
+
+#if 1
+    std::cout << "## LEFT" << std::endl;
+    print_shifts(move_left_possible, left_shift, cars_amount, first_car);
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "## RIGHT" << std::endl;
+    print_shifts(move_right_possible, right_shift, cars_amount, first_car);
+    std::cout << std::endl;
+    std::cout << std::endl;
+#endif
+
+    // use produced tables
+    for(int i = 0; i < cars_amount; ++i) {
+        // move left
+        std::vector<int> left_shift_req(cars_amount);
+        bool             left_ok = get_required_shifts(move_left_possible[i], left_shift[i], cross_car_lookup[i].second, left_shift_req);
+        // move right
+        std::vector<int> right_shift_req(cars_amount);
+        bool             right_ok = get_required_shifts(move_right_possible[i], right_shift[i], !cross_car_lookup[i].second, right_shift_req);
+
+        std::cout << static_cast<char>(first_car + i) << ": ";
+        std::vector<std::string> out;
+        // select better move
+        // either left is better than right or right is impossible
+        if(left_ok && (better_shift(left_shift_req, right_shift_req) || !right_ok)) {
+            for(int x = 0; x < left_shift_req.size(); ++x)
+                if(left_shift_req[x]) {
+                    std::stringstream ss;
+                    ss << cross_car_lookup[x].first << " " << left_shift_req[x] << " links";
+                    out.push_back(ss.str());
+                    // move to pace after current car
+                    ++x;
+                }
+        }
+        else if(right_ok) {
+            // go the other way around <- now moving right
+            for(int x = right_shift_req.size() - 1; x > 0; --x)
+                if(right_shift_req[x]) {
+                    std::stringstream ss;
+                    ss << cross_car_lookup[x].first << " " << right_shift_req[x] << " rechts";
+                    out.push_back(ss.str());
+                    --x;
+                }
+        }
+        else {
+            std::cout << "unmoeglich" << std::endl;
+            continue;
+        }
+        for(int i = 0; i < out.size(); ++i) {
+            std::cout << out[i];
+            if(i < out.size() - 1)
+                std::cout << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ submission/aufgabe2/src/main.cpp
@@ -0,0 +1,147 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#define inf std::numeric_limits<float>::max() / 2
+
+constexpr int DAYS    = 5;
+constexpr int DAY_LEN = 6 * 60;
+
+void populate_tables(
+    const std::vector<std::pair<int, float>>& hotels,
+    std::vector<std::vector<float>>&          min_ratings,
+    std::vector<std::vector<int>>&            last_hotel)
+{
+    // the "first" (virtual) hotel never has the worst rating
+    min_ratings[0][0] = inf;
+    // go through all but "first" hotels
+    for(auto cur = hotels.begin() + 1; cur != hotels.end(); ++cur) {
+        int cur_idx = cur - hotels.begin();
+        // first hotel `cur` can be reached from
+        auto prev = std::lower_bound(hotels.begin(), hotels.end(),
+                                     std::make_pair(cur->first - DAY_LEN, .0f));
+        for(; prev != cur; ++prev) {
+            int prev_idx = prev - hotels.begin();
+            // go through past days when prev can be reached
+            // call ride off after `DAYS` days
+            for(int prev_day = 0, cur_day = 1; prev_day < DAYS; ++prev_day, ++cur_day) {
+                // if the hotel `prev` can't be reached in `prev_day` days
+                if(min_ratings[prev_idx][prev_day] == -1)
+                    continue;
+                float new_min = std::min(min_ratings[prev_idx][prev_day], cur->second);
+                // update when better
+                if(new_min > min_ratings[cur_idx][cur_day]) {
+                    min_ratings[cur_idx][cur_day] = new_min;
+                    last_hotel[cur_idx][cur_day]  = prev_idx;
+                }
+            }
+        }
+    }
+}
+
+int get_best_day(const std::vector<std::vector<float>>& min_ratings, int n)
+{
+    int   best_day {-1};
+    float best_min_rating {-1};
+    for(int i = 0; i < DAYS + 1; ++i) {
+        if(min_ratings[n + 1][i] > best_min_rating) {
+            best_min_rating = min_ratings[n + 1][i];
+            best_day        = i;
+        }
+    }
+    return best_day;
+}
+
+void construct_path(
+    const std::vector<std::vector<int>>& last_hotel,
+    int                                  best_day,
+    int                                  n,
+    std::vector<int>&                    path)
+{
+    int cur_idx = last_hotel[n + 1][best_day];
+    for(int cur_day = best_day - 1; cur_day; --cur_day) {
+        path.push_back(cur_idx);
+        cur_idx = last_hotel[cur_idx][cur_day];
+    }
+    std::reverse(path.begin(), path.end());
+}
+
+int main()
+{
+    int n, t;
+    std::cin >> n >> t;
+    // location, rating
+    std::vector<std::pair<int, float>> hotels(n + 2);
+    // first "hotel" is start
+    hotels[0] = {0, inf};
+    // last "hotel" is end
+    hotels[n + 1] = {t, inf};
+    for(int i = 1; i <= n; ++i) {
+        std::cin >> hotels[i].first >> hotels[i].second;
+    }
+
+    // min_ratings[x][y] -> min rating till hotel x requiring y days to get to
+    // -1 -> impossible
+    std::vector<std::vector<float>> min_ratings(n + 2, std::vector<float>(DAYS + 1, -1));
+    std::vector<std::vector<int>>   last_hotel(n + 2, std::vector<int>(DAYS + 1, -1));
+
+    populate_tables(hotels, min_ratings, last_hotel);
+
+    int best_day = get_best_day(min_ratings, n);
+
+    // reconstruct path
+    std::vector<int> path;
+    if(best_day != -1)
+        construct_path(last_hotel, best_day, n, path);
+
+    // print table header
+    std::cout << "   \t               \t  min rating at day:" << std::endl;
+    std::cout << "idx\tdistance rating\t  ";
+    for(int i = 0; i <= DAYS; ++i)
+        std::cout << i << "\t";
+    std::cout << std::endl;
+    std::cout << "===\t======== ======\t  ";
+    for(int i = 0; i <= DAYS; ++i)
+        std::cout << "=\t";
+    std::cout << std::endl;
+
+    // print table body
+    for(int hotel_id = 0; hotel_id < n + 2; ++hotel_id) {
+        std::cout << hotel_id << "\t";
+        std::cout << hotels[hotel_id].first << "\t ";
+        if(hotels[hotel_id].second == inf)
+            std::cout << "inf\t";
+        else
+            std::cout << hotels[hotel_id].second << "\t";
+        if(std::find(path.begin(), path.end(), hotel_id) != path.end())
+            std::cout << "+ ";
+        else
+            std::cout << "  ";
+        for(float x: min_ratings[hotel_id]) {
+            if(x == -1)
+                std::cout << " \t";
+            else if(x == inf)
+                std::cout << "inf\t";
+            else
+                std::cout << x << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    if(best_day == -1) {
+        std::cout << "It is impossible to reach the destination in " << DAYS << " days without draining the phones' batteries." << std::endl;
+        std::cout << "Maybe read a book instead." << std::endl;
+    }
+    else {
+        // print path
+        std::cout << "These hotels should be used with min rating " << min_ratings[n + 1][best_day] << ":" << std::endl;
+        std::cout << "idx\tlocation\trating\tmin rating till here" << std::endl;
+        std::cout << "===\t========\t======\t====================" << std::endl;
+        for(int day = 1; day < DAYS && day - 1 < path.size(); ++day) {
+            int idx = path[day - 1];
+            std::cout << idx << "\t" << hotels[idx].first << "\t\t" << hotels[idx].second << "\t" << min_ratings[idx][day] << std::endl;
+        }
+    }
+}
