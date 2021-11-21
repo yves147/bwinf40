490f320caa886f097f0196d7be542110c4f2d9db
yves147 <yves.j@outlook.de>
2021-11-21 20:55:20 +0100

    _

--- task_3/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for k in {0..6}
-do
-	for i in {0..2}
-	do
-	echo "$k - $i"
-	time (cat examples/worte$k.txt | ./a.out $i)
-	done
-done
--- task_3/launch.sh
+++
@@ -1,2 +0,0 @@
-g++ main.cpp -O3 -o generator
-./generator word_list
