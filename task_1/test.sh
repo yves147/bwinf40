8696f4dab9cf0cb94c71f0ee85d7083ee04d5746
christopher-besch <christopher.besch@gmx.de>
2021-11-21 20:43:37 +0100

    cleaned task_1 and task_2

--- task_1/test.sh
+++ task_1/test.sh
@@ -5,5 +5,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 g++ -Wall -Wextra -Wshadow -fsanitize=undefined,address -D_GLIBCXX_DEBUG -g main.cpp
 echo done
-cat test.txt | ./a.out
+cat ${DIR}/examples/parkplatz0.txt | ./a.out
 
