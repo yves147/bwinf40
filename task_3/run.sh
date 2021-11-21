f7ee622f5a705ae09d99ddad2fa6fd6cdbdafd08
christopher-besch <christopher.besch@gmx.de>
2021-11-21 20:37:39 +0100

    updated gitignore

--- task_3/run.sh
+++ task_3/run.sh
@@ -5,5 +5,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 g++ -Wall -Wextra -Wshadow -fsanitize=undefined,address -D_GLIBCXX_DEBUG -g main.cpp
 echo done
-cat word_list | ./a.out
+cat examples/worte4.txt | ./a.out
 
